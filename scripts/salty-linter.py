6b67821ea72c9e14a01481a789d4b6330f3b26e3
saltydk <salty@salty.dk>
2024-02-04 00:12:03 +0100

    scripts: update print format

--- scripts/salty-linter.py
+++ scripts/salty-linter.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-def lint_ansible_defaults(content):
+def lint_ansible_defaults(content, file_path):
     errors = []
     lines = content.split('\n')
     
@@ -21,7 +21,8 @@ def lint_ansible_defaults(content):
         elif within_multi_line_jinja:
             if 'if' in stripped_line or 'else' in stripped_line:
                 if line.find('if') < multi_line_jinja_start and line.find('else') < multi_line_jinja_start:
-                    errors.append(f"Line {line_no}: 'if/else' within Jinja expression should align with the start.")
+                    message = f"'if/else' within Jinja expression should align with the start."
+                    errors.append((line_no, message))
 
             if '}}' in stripped_line:
                 within_multi_line_jinja = False
@@ -29,7 +30,11 @@ def lint_ansible_defaults(content):
         if '}}' in stripped_line and within_multi_line_jinja:
             within_multi_line_jinja = False
 
-    return errors
+    for error in errors:
+        line_no, message = error
+        print(f"::warning file={file_path},line={line_no},endLine={line_no},title=Salty Lint Error::{message}")
+
+    return len(errors) > 0
 
 def crawl_and_lint_ansible_roles(roles_dir):
     errors_found = False
@@ -43,15 +48,11 @@ def crawl_and_lint_ansible_roles(roles_dir):
         if os.path.isfile(defaults_main_path):
             with open(defaults_main_path, 'r') as file:
                 content = file.read()
-                errors = lint_ansible_defaults(content)
-                if errors:
+                if lint_ansible_defaults(content, defaults_main_path):
                     errors_found = True
-                    for error in errors:
-                        print(f"{defaults_main_path}: {error}")
 
     sys.exit(1 if errors_found else 0)
 
-# Check if the script was run with a directory argument
 if len(sys.argv) < 2:
     print("Usage: python script.py /path/to/your/ansible/roles")
     sys.exit(1)
